fbc3573c9f3122ee1cc4e0328cdb8f4a1ecdf48b
Roy Onrust <roy.onrust@gmail.com>
2025-10-09 08:37:08 +0200

    qwewaeesa
    dasdsa

---
+++ Assets/ULmanager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ULmanager : MonoBehaviour
+{
+    public bool key;
+    public bool battery;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
